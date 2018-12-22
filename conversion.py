1cbc7b128553dff9c615b85c02b137a9667cac52
BaisongNing <bsning@gmail.com>
2018-12-22 16:22:19 +0800

    Add 1 more function in python file

--- conversion.py
+++ conversion.py
@@ -1,5 +1,9 @@
-> # My Conversion Tools
->
-> def dollars2cents(dollars):
->     cents = dollars 
->     return cents
+ # My Conversion Tools
+
+ def dollars2cents(dollars):
+     cents = dollars 
+     return cents
+
+ def gallons2liters(gallons):
+    liters = gallons * 3.78541
+    return liter
